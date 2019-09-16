c0c3993a0c5cea5542f9efa9bded5aa50fcf20d9
Ramy Wassef <ramywroshdy@gmail.com>
2019-09-16 10:26:46 +0200

    Updates

---
+++ precache-manifest.57efeae3ccccba614943ae34a362aed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f1a4b9098889fca93f7c596276afc3",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "3cc6ca25337bd9c0efa2",
+    "url": "/Todo-list/static/css/main.7060c710.chunk.css"
+  },
+  {
+    "revision": "b19158384adb2e63255d",
+    "url": "/Todo-list/static/js/2.96dc2f2b.chunk.js"
+  },
+  {
+    "revision": "3cc6ca25337bd9c0efa2",
+    "url": "/Todo-list/static/js/main.f0ce2f30.chunk.js"
+  },
+  {
+    "revision": "9ba2c5c6e60f702007d5",
+    "url": "/Todo-list/static/js/runtime~main.e9ad53d7.js"
+  }
+]);
